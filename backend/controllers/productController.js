f887a347e6949fc712c7f1ef4d441798801cce63
Malta <marcelmalta@hotmail.com>
2025-08-04 21:16:31 -0300

    feat: ajustes em forum.ejs e controllers para fórum com comentários

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,2 +1,8 @@
-// Controller vazio para evitar erro de módulo não encontrado
-module.exports = {};
+// backend/controllers/productController.js
+
+module.exports = {
+  listProducts: (req, res) => res.send('Em breve: lista de produtos!'),
+  adminListProducts: (req, res) => res.send('Em breve: admin lista de produtos!'),
+  renderNewProduct: (req, res) => res.send('Em breve: formulário novo produto!'),
+  createProduct: (req, res) => res.send('Em breve: criar produto!'),
+};
