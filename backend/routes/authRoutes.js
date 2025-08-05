bb6ab04121004f5a7fb90ec0a4d5fdb95a3acbde
Malta <marcelmalta@hotmail.com>
2025-08-05 08:22:56 -0300

    Adiciona rotas públicas de login e cadastro

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Pode adicionar rotas de autenticação depois!
+// ROTA GET LOGIN (Renderiza a página de login)
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// ROTA GET CADASTRO (Renderiza a página de cadastro)
+router.get('/cadastro', (req, res) => {
+  res.render('register');
+});
 
 module.exports = router;
